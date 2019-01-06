3d3803fa16e30c7640e5b53365e76db7f8bbef3b
Slajni <esinnley@gmail.com>
2019-01-06 21:06:06 +0100

    added ball and paddle to vector

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 #include "Game.h"
 #include "MapObject.h"
 #include "Paddle.h"
+#include "Ball.h"
 
 
 int main()
 {
+	srand(time(NULL));
 	sf::RenderWindow window(sf::VideoMode(800, 800), "Insane Balls");
 	window.setFramerateLimit(60);
 	window.setMouseCursorVisible(false);
@@ -16,9 +22,13 @@ int main()
 	Game * game = new Game(&window);
 	sf::Texture paddleText;
 	paddleText.loadFromFile("images/paddle.png");
+	sf::Texture ballText;
+	ballText.loadFromFile("images/ball.png");
 
+	Paddle * paddle = new Paddle(paddleText, &window, sf::Vector2i(90, 9));
 
-	game->addMapObject(new Paddle(paddleText,&window,sf::Vector2i(90,9)));
+	game->addMapObject(paddle);
+	game->addMapObject(new Ball(ballText, &window, 4, 4));
 
     while (window.isOpen())
     {
