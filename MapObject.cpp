140e4250e4a9f8478d823593b7782978e477be74
Slajni <esinnley@gmail.com>
2019-01-06 17:07:29 +0100

    update constructor, implement gParentWindow getter method and change position method

--- MapObject.cpp
+++ MapObject.cpp
@@ -6,8 +6,9 @@ MapObject::MapObject()
 {
 }
 
-MapObject::MapObject(const sf::Texture & text, int x, int y)
+MapObject::MapObject(const sf::Texture & text, int x, int y, sf::RenderWindow * window)
 {
+	this->parentWindow = window;
 	this->texture = text;
 	this->startX = x;
 	this->startY = y;
@@ -27,11 +28,25 @@ sf::Sprite MapObject::gSprite()
 	return *this->sprite;
 }
 
+sf::RenderWindow * MapObject::gParentWindow()
+{
+	return this->parentWindow;
+}
+
+void MapObject::changePosition(int x, int y)
+{
+	this->sprite->setPosition(x, y);
+}
+
 void MapObject::move(float x, float y, float dx, float dy, float multiplierX, float multiplierY)
 {
 	{
 		sprite->move(sf::Vector2f(x * multiplierX * dx, y * multiplierY * dy));
 	}
+}
+
+void MapObject::update()
+{
 }
 
 int MapObject::nextId = 0;
